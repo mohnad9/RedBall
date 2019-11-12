e9ca1a28ef842d129ae9a4667e6290643d0492c7
mohnad9 <51960659+mohnad9@users.noreply.github.com>
2019-11-12 12:44:11 +0200

    part 1
    part 1

---
+++ Player.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public Joystick joystick;
+    public Rigidbody2D rb;
+
+    public float Speed = 10;
+
+
+    void Update()
+    {
+        if(joystick.Horizontal > 0)
+        {
+            rb.position += Vector3.right * Speed * Time.fixedDeltaTime;
+
+        }
+       
+       else if(joystick.Horizontal < 0)
+        {
+            rb.position -= Vector3.right * Speed * Time.fixedDeltaTime;
+
+
+        }
+    }
+}
